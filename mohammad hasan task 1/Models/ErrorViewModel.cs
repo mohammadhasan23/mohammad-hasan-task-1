7cdf6d6b89f3907809b4a9c38ca30ef623551278
PC <PC@DESKTOP-RMF4VC6>
2025-09-16 10:58:19 +0300

    Add project files.

---
+++ mohammad hasan task 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mohammad_hasan_task_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
